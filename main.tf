f849a8c29a1e7d0d7c1c82e36032faa71f1d36a2
Elise Ching Sze Lin <50040281+eliseching99@users.noreply.github.com>
2023-06-02 13:37:16 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,50 +1,40 @@
-# # Copyright (c) HashiCorp, Inc.
-# # SPDX-License-Identifier: MPL-2.0
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
 
-# provider "aws" {
-#   region = var.region
-# }
+provider "aws" {
+  region = var.region
+}
 
-# data "aws_ami" "ubuntu" {
-#   most_recent = true
+data "aws_ami" "ubuntu" {
+  most_recent = true
 
-#   filter {
-#     name   = "name"
-#     values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-#   }
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
 
-#   filter {
-#     name   = "virtualization-type"
-#     values = ["hvm"]
-#   }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
 
-#   owners = ["099720109477"] # Canonical
-# }
+  owners = ["099720109477"] # Canonical
+}
 
-# resource "aws_instance" "ubuntu" {
-#   ami           = data.aws_ami.ubuntu.id
-#   instance_type = var.instance_type
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
 
-#   tags = {
-#     Name = var.instance_name
-#   }
-# }
+  tags = {
+    Name = "Elise second ec2"
+  }
+}
 
+resource "aws_s3_bucket" "example" {
+  bucket = "elise-test-bucket"
 
-# resource "aws_instance" "web" {
-#   ami           = data.aws_ami.ubuntu.id
-#   instance_type = "t2.micro"
-
-#   tags = {
-#     Name = "Elise second ec2"
-#   }
-# }
-
-# resource "aws_s3_bucket" "example" {
-#   bucket = "elise-test-bucket"
-
-#   tags = {
-#     Name        = "My bucket"
-#     Environment = "Dev"
-#   }
-# }
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
