23b6256785aaeba851894fe7e08d049bf24b7ac9
Elise Ching Sze Lin <50040281+eliseching99@users.noreply.github.com>
2023-06-02 18:52:39 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -30,7 +30,7 @@ resource "aws_instance" "web" {
   }
 }
 
-resource "aws_instance" "web" {
+resource "aws_instance" "web2" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t3.micro"
 
